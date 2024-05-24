a6c00cf449fd911aa4a59d53c2747f0c10cc16b0
rrlc <ladvocatrodrigo@gmail.com>
2024-05-24 00:31:24 -0300

    deleting current db

--- prisma/migrations/20240523191529_buys_table/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateTable
-CREATE TABLE "Bought" (
-    "userId" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    "productName" TEXT NOT NULL,
-    "productPrice" INTEGER NOT NULL,
-    "quantity" INTEGER NOT NULL,
-    CONSTRAINT "Bought_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
-);
